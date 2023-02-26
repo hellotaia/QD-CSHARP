f7cfc9f74a926f25624f208a971cfdb592ab497a
Taisiia Teslenko <hellotaia@gmail.com>
2023-02-25 22:19:51 -0300

    init

---
+++ OOP-3-Constructors/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace OOP_3_Constructors
+{
+    public class Messenger
+    {
+        //создать класс Messenger;
+        //создать статическое поле State;
+        public static string State;
+        //создать поле message, messageCounter;
+        public string message;
+        public int messageCounter;
+        //создать 2 конструктора: static Messenger (), public Messenger (string message, int messageCounter);
+
+        static Messenger()
+        {
+            //в статическом конструкторе присвоить значение статическому полю класса State;
+            State = "ready";
+        }
+
+        public Messenger(string message, int messageCounter)
+        {
+            //в конструкторе с параметрами присвоить значения полям класса message и messageCounter с помощью this;
+            this.message = message;
+            this.messageCounter = messageCounter;
+        }
+
+        public void Deconstruct(out string _message, out int _msgcount)
+        {
+            _message = message;
+            _msgcount = messageCounter;
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создать экземпляры класса Messenger, используя оба конструктора;
+            var staticObject = Messenger.State;
+            Console.WriteLine($"Messenger State: {Messenger.State}");
+            Messenger msgr2 = new Messenger("new message", 2);
+
+            //с помощью деконструктора присвоить переменной значение поля message;
+            (string _deconstructMessage, int messageCounter) = msgr2;
+            Console.WriteLine(_deconstructMessage);
+            Console.WriteLine(messageCounter);
+        }
+    }
+}
---
+++ OOP-4-Incapsulation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace OOP_4_Incapsulation
+{
+    public class Coffee
+    {
+        //создать класс Coffee с 2 приватными полями: _name, _price;
+        //для полей _name и _price создать свойства set. В свойстве для цены проверить, чтобы значение было > 0;
+        private string _name;
+        private decimal _price;
+
+        public string Name
+        {
+            set { _name = value; }
+        }
+        public decimal Price
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    _price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Price should be greater than zero");
+                }
+            }
+        }
+        //создать конструктор класса Coffee с параметрами name и price, проинициализировать поля класса;
+        public Coffee(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        //создать приватный метод GetInfo(), который будет выводить информацию о напитке;
+        private void GetInfo()
+        {
+            Console.WriteLine($"name: {_name}, price: {_price} hrn");
+        }
+
+        //создать публичный метод PrintInfo(), который будет вызывать приватный метод и добавлять в конце строку ;
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Calling private method...");
+            GetInfo();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //создать экземпляр класса Coffee. У этого экземпляра класса вызвать метод PrintInfo().
+            Coffee coffee = new Coffee("Espresso", -2);
+            coffee.PrintInfo();
+        }
+    }
+}
